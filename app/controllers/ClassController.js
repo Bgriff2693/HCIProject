8cbdb54ce0ba64fd802ec6ee0f84b5d4a1c92b4c
Nick Fox <nickfox25@gmail.com>
2015-11-03 15:44:42 -0600

    add data from json

--- app/controllers/ClassController.js
+++ app/controllers/ClassController.js
@@ -1,8 +1,12 @@
 angular.module('appControllers', [])
-  .controller('MainController', [function() {
+  .controller('MainController', ['$http', function($http) {
     vm = this;
 
-    
+    $http.get('database/degree_plan.JSON').success(function(data) {
+      this.data = data;
+    })
+
+
   }])
   .controller('MenuController', [function() {
 
