666d87db54d071e94a4e55a72f0fd349e0fdc6d1
Jeremy Moorman <jsmoorman@tamu.edu>
2015-11-04 18:00:42 -0600

    Fixed small bug where Systems track showed up in other track

--- app/controllers/appControllers.js
+++ app/controllers/appControllers.js
@@ -1,18 +1,28 @@
 angular.module('appControllers', [])
     .controller('degreeController', ['$http', '$scope', function($http, $scope) {
-        vm = this;
         $scope.courses = null;
 
+        // Gets the current courses in the json file and puts it in the scope
         $http.get('app/database/degree_plan.JSON')
             .success(function(data) {
                 $scope.courses = data.courses;
-                console.log(data);
+                console.log("Courses: " + data);
             })
             .error(function (data, status, headers, config) {
-                console.log("ERROR, ERROR, ERROR");
+                console.log("ERROR: Could not get courses");
             });
     }])
 
+    .controller('sectionController', ['$scope', function($scope) {
+
+        $scope.init = function(cid)
+        {
+            // This function is sort of private constructor for controller
+            // You can pass the CID to this through ng-init and use it in the controller
+            $scope.cid = cid;
+        };
+
+    }])
     .controller('MenuController', [function() {
 
     }]);
